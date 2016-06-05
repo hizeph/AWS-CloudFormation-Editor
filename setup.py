31b97d78f687afe96b3666f30bd9da1941f5a5c9
Cezar <cezar.a.bernardi@gmail.com>
2016-06-05 18:52:43 -0300

    base script template

--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
-import sys, argparse
+
+import sys, argparse, subprocess
+
+def node_config ():
+	print("node_config")
+	pass
+
+def net_config ():
+	print("net_config")
+	pass
+
+def save_config ():
+	print("save_config")
+	pass
 
 def overall_conf ():
 	print("\nConfiguration Options:\n")
 	print("\t1 - Node configuration")
 	print("\t2 - Network configuration")
+	print("\t3 - Save configuration")
 	print("")
+	read_input = input("Type option number: ")
+	read_input = int(read_input)
+	try:
+		conf_dict[read_input]()
+	except:
+		overall_conf()
 
+conf_dict = {
+	1: node_config,
+	2: net_config,
+	3: save_config }
 
-def init (argv):
+def setup (argv):
 	parser = argparse.ArgumentParser(description = "Manage AWS CloudFormation Template")
 	parser.add_argument('action', choices = (
 		'std',
@@ -16,14 +40,12 @@ def init (argv):
 	if args.action == 'std':
 		# Load all std
 		print ("std")
-		pass
 	elif args.action == 'conf':
 		# Load std and display options
 		overall_conf()
-		pass
 	else:
 		assert False
 
 
 if __name__ == '__main__':
-    init(sys.argv)
+    setup(sys.argv)
