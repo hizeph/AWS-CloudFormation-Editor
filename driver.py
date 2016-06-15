fbb5cc73e791ed89ad09954454d65542214987c9
Cezar <cezar.a.bernardi@gmail.com>
2016-06-15 01:42:46 -0300

    missing extra files

--- driver.py
+++ driver.py
@@ -1,8 +1,10 @@
+import json
+
 class InfrastructureConfiguration:
 
 	def __init__ (self):
 		self.n_nodes = 3
-		self.packages = []
+		self.packages = ["openmpi", "openmpi-devel", "gcc", "gcc-c++", "ant", "java-1.8.0-openjdk-devel"]
 		self.files = []
 
 	def set_n_nodes (self, n_nodes):
@@ -38,33 +40,59 @@ class InfrastructureConfiguration:
 		return host_file
 
 	def create_package_list (self):
-		pass
+		package_list = { "packages" : {"yum" : {}} }
+		for package in self.packages:
+			package_list['packages']['yum'][package] = []
+		#print(json.dumps(package_list))
+		return package_list
+
 
 	def create_extra_files (self):
 		pass
 
-	def create_nodes(self, ip_map, host_file, package_list, extra_files):
-		#node['Properties']['NetworkInterfaces'][0]['PrivateIpAddress']['Fn::FindInMap'][1] = "Node01"
-		#UserData = "StackNode01"
-		#Tags = "Node01"
-		pass
+	def create_nodes(self, host_file, package_list, extra_files):
+
+		node_list = []
+		for n in range(1,self.n_nodes+1):
+			if (n < 10):
+				node_resource_name = "StackNode0" + str(n)
+				node_name = "Node0" + str(n)
+			else:
+				node_resource_name = "StackNode" + str(n)
+				node_name = "Node" + str(n)
+			try:
+				node = json.loads(open("Templates/node.json").read())
+				node['Properties']['NetworkInterfaces'][0]['PrivateIpAddress']['Fn::FindInMap'][1] = node_name
+				node['Properties']['UserData']['Fn::Base64']['Fn::Join'][1][5] = "         --resource " + node_resource_name
+				node['Properties']['Tags'].append({"Key" : "Name", "Value" : node_name})
+				node['Metadata']['AWS::CloudFormation::Init']['setup'].update(package_list)
+				node['Metadata']['AWS::CloudFormation::Init']['setup']['files'].update(host_file)
+				node_list.append(node)
+			except:
+				print ("Error creating node")
+		return node_list
 
 
-	def generate_infra (self):
+	def generate_infra (self, output_name):
 		ip_map = self.create_ip_map()
 		host_file = self.create_hosts_file()
-		#self.create_package_list()
-		#self.create_extra_files()
-		#self.create_nodes(ip_map, host_file, package_list, extra_files)
+		package_list = self.create_package_list()
+		extra_files = {} #self.create_extra_files()
+		node_list = self.create_nodes(host_file, package_list, extra_files)
 		try:
 			base_infra = json.loads(open("Templates/base_infra.json").read())
-			node = json.loads(open("Templates/node.json").read())
 		except:
 			print ("Error loading files")
+
+		base_infra['Mappings'].update(ip_map)
 		for n in range(1,self.n_nodes+1):
 			if (n < 10):
 				node_resource_name = "StackNode0" + str(n)
 			else:
 				node_resource_name = "StackNode" + str(n)
 
-			#base_infra['Properties'][node_resource_name]
+			base_infra['Resources'][node_resource_name] = node_list[n-1]
+		
+		infra_file = open(output_name, "w")
+		infra_file.write(json.dumps(base_infra))
+		infra_file.close()
--- setup.py
+++ setup.py
@@ -1,37 +1,65 @@
 
-import sys, argparse, json
-from configurator import InfrastructureConfiguration
+import sys
+from driver import InfrastructureConfiguration
 
-def overall_conf ():
-	print("\nConfiguration Options:\n")
-	print("\t1 - Number of nodes")
-	print("\t2 - Packages to install")
-	print("\t3 - Custom files")
-	print("\t4 - Save configuration")
-	print("")
-	read_input = input("Type option number: ")
-	read_input = int(read_input)
+class Setup:
 
+	def __init__(self):
+		self.conf = InfrastructureConfiguration()
+		self.conf_map = {
+			1: self.conf_nodes,
+			2: self.conf_packages,
+			4: self.show_conf,
+			5: self.save_conf,
+			6: sys.exit
+		}
 
-def setup (argv):
-	conf = InfrastructureConfiguration()
-	conf.create_hosts_file()
-	'''
-	parser = argparse.ArgumentParser(description = "Manage AWS CloudFormation Template")
-	parser.add_argument('action', choices = (
-		'std',
-		'conf'))
-	args = parser.parse_args(argv[1:])
-	if args.action == 'std':
-		# Load all std
-		print ("std")
-	elif args.action == 'conf':
-		# Load std and display options
-		overall_conf()
-	else:
-		assert False
-	'''
+	def conf_nodes (self):
+		try:
+			read_input = input("\nTotal desired number of nodes: ")
+			self.conf.set_n_nodes(int(read_input))
+		except KeyboardInterrupt:
+			self.overall_conf()
+
+	def conf_packages (self):
+		try:
+			read_input = input("\nPackage name to add: ")
+			self.conf.add_package(str(read_input))
+		except KeyboardInterrupt:
+			self.overall_conf()
+
+	def show_conf (self):
+		print("\nNumber of nodes: " + str(self.conf.n_nodes))
+		print("Packages to install:");
+		for package in self.conf.packages:
+			print("\t" + str(package))
+
+	def save_conf (self):
+		try:
+			read_input = input("\nName of the output file: ")
+			self.conf.generate_infra(str(read_input))
+			print("File saved")
+		except KeyboardInterrupt:
+			self.overall_conf()
+
+	def overall_conf (self):
+		while(True):
+			try:
+				print("\nConfiguration Options:\n")
+				print("\t1 - Number of nodes")
+				print("\t2 - Packages to install")
+				print("\t3 - Custom files")
+				print("\t4 - Show configuration")
+				print("\t5 - Save configuration")
+				print("\t6 - Quit")
+				print("")
+				read_input = input("Type option number: ")
+				self.conf_map[int(read_input)]()
+			except KeyboardInterrupt:
+				print("")
+				break
 
 
 if __name__ == '__main__':
-	setup(sys.argv)
+	setup = Setup()
+	setup.overall_conf()
